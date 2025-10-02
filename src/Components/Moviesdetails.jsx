9b8842cd4bd10815e3e2ab43c8f067a743ec336c
Jkk7595 <jospeterkananu7595@gmail.com>
2025-10-02 11:22:49 +0300

    second commit

--- src/Components/Moviesdetails.jsx
+++ src/Components/Moviesdetails.jsx
@@ -1,6 +1,10 @@
-
-
-
+import audienceFresh from "../assets/audience-fresh.jpg" 
+import audienceRotten from"../assets/audience-rotten.jpg"
+import criticCertified from "../assets/critic-certified.jpg"
+import criticFresh from "../assets/critic-fresh.jpg"
+import criticRotten from "../assets/critic-rotten.jpg"
+import React from "react"
+import { movies } from "../assets/movies.js"
 function Moviesdetails(){
     return(
 
@@ -26,11 +30,17 @@ function Moviesdetails(){
             <div>
                 <p>New & upcoming movies in theatre</p>
                 <p>View All</p>
+              {movies.map((movie)=>(
+                        // console.log(movies)
+                        
+                        <div key={movie.name}>
+                            <img src={movie.image} alt={movie.name} />
+                            <img src={audienceFresh} />
+
+                        </div>
+              ))}
               
-                {movies.map((item)=(
-                  console.log(movies)
-                  // <img src={item.image} alt={item.name} />
-                ))}
+               
             </div>
         </>
             )
